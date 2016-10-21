0e394207dd96c3773831151a16deea444aa3160e
Diego David <diegodavidq@outlook.com>
2016-10-21 18:06:24 -0500

    PHP Hello World

---
+++ 0_1EtiquetasPHP.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Etiquetas PHP</title>
+</head>
+<body>
+
+<?php echo "Codigo de  PHP1" ?>
+<script language="php"> echo "Codigo de PHP 2" </script>
+
+</body>
+</html>
+
