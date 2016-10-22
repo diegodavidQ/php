19bb183b0d466c74c8f1b4a8dc613092529f7d1a
Diego David <diegodavidq@outlook.com>
2016-10-21 20:21:33 -0500

    Agregado comentarios

---
+++ 1_ComentariosCortoPHP.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Etiquetas PHP</title>
+</head>
+<body>
+
+<?
+# Primer comentario
+# Segunda línea de comentario
+// Tercer comentario
+print "Ejemplo con comentarios cortos en PHP";
+?>
+
+</body>
+</html>
+
